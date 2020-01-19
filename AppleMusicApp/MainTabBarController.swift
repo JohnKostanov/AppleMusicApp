3bf409cb2cf8a6b10bf6d5c3ad2a0931990a3cbd
JohnKostanov <kostanovd@icloud.com>
2020-01-19 20:46:25 +0100

    Add class MainTabBarController

---
+++ AppleMusicApp/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  AppleMusicApp
+//
+//  Created by  Джон Костанов on 19/01/2020.
+//  Copyright © 2020 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
