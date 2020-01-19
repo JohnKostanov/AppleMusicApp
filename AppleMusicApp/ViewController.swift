c7c6021d0f17036f7ebb153dfdf6962d63531eb5
JohnKostanov <kostanovd@icloud.com>
2020-01-19 20:36:41 +0100

    Initial Commit

---
+++ AppleMusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMusicApp
+//
+//  Created by  Джон Костанов on 19/01/2020.
+//  Copyright © 2020 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
