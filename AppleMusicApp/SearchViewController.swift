18e8710289227b57936488c17715405983a3e525
JohnKostanov <kostanovd@icloud.com>
2020-01-20 20:07:58 +0100

    Add UITableViewCell

--- AppleMusicApp/SearchViewController.swift
+++ AppleMusicApp/SearchViewController.swift
@@ -15,5 +15,17 @@ class SearchViewController: UITableViewController {
         
         view.backgroundColor = .white
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.textLabel?.text = "indexPath: \(indexPath)"
+        return cell
     }
 }
