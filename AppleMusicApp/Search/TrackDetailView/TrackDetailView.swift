72be628c64ffaab9c5cd60003d2b37c39ffe4229
JohnKostanov <kostanovd@icloud.com>
2020-01-25 14:46:57 +0100

    Add Track Detail Controller

---
+++ AppleMusicApp/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackDetailView.swift
+//  AppleMusicApp
+//
+//  Created by  Джон Костанов on 25/01/2020.
+//  Copyright © 2020 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+
+class TrackDetailView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
