c43f057e56c43c31addc677d0627dbde1d337d4a
JohnKostanov <kostanovd@icloud.com>
2020-01-28 23:44:23 +0100

    Add Dynamic List with Tracks and new fraimwork URL Image for SwiftUI with Swift Package

---
+++ AppleMusicApp/Search/Cell/UserDefaults.swift
@@ -0,0 +1,24 @@
+//
+//  UserDefaults.swift
+//  AppleMusicApp
+//
+//  Created by  Джон Костанов on 28/01/2020.
+//  Copyright © 2020 John Kostanov. All rights reserved.
+//
+
+import Foundation
+
+
+extension UserDefaults {
+    
+    static let favouriteTrackKey = "favouriteTrackKey"
+    
+    func savedTracks() -> [SearchViewModel.Cell] {
+        let defaults = UserDefaults.standard
+        
+        guard let savedTracks = defaults.object(forKey: UserDefaults.favouriteTrackKey) as? Data else { return [] }
+        guard let decodedTracks = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedTracks) as? [SearchViewModel.Cell] else { return [] }
+        return decodedTracks
+    }
+    
+}
